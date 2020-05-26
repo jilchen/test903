3435603c8b5ffff0e1ff30f8b9fb171be8355841
jilchen <jil903@rses.tc.edu.tw>
2020-05-26 12:25:23 +0000

    0526

--- 00560525.R
+++ 00560525.R
@@ -3,7 +3,6 @@ rm(list=ls())
 con = gzcon(url('https://github.com/systematicinvestor/SIT/raw/master/sit.gz', 'rb'))
 source(con)
 close(con)
-
 #
 install.packages('pacman')
 library(pacman)
@@ -14,29 +13,30 @@ p_load(quantmod, xts)
 # etf56 <- read.csv("tw0056_2007_2019_03.csv", header = T)
 # etf56 <- read.delim("tw0056_2007_2019_01.csv", header = T)
 # etf56 <- read.table("tw0056_2007_2019.txt", header = TRUE)
-etf56 <- read.table("Backtesting-master/tw0056_2007_2019.txt", header = TRUE)
+etf56 <- read.table("005620072019.txt", header = TRUE)
 #etf56 <- read.csv("tw0056_20070101_20191231.csv", header = TRUE)
 #etf50 <- read.table("tw50_20030630_20181231.txt", header = TRUE)
 etf56 <- etf56[c(3, 7)]
-colnames(etf56) <- c("date", "tw56")
+colnames(etf56) <- c("date", "price")
 head(etf56)
 tail(etf56)
 str(etf56)
-sum(is.na(etf56$tw56))
+#檢查是否有na值
+sum(is.na(etf56$price))
 #
 # convert to time series data
 # library(lubridate)
 # etf50.xts <- xts(etf50$tw50, order.by = ymd(as.character(etf50$date)))
-etf56.xts <- xts(etf56$tw56, order.by = as.Date(as.character(etf56$date), format = "%Y%m%d"))
+etf56.xts <- xts(etf56$price, order.by = as.Date(as.character(etf56$date), format = "%Y%m%d"))
 head(etf56.xts)
-colnames(etf56.xts) <- 'tw56'
+colnames(etf56.xts) <- 'price'
 head(etf56.xts)
 #
 data <- new.env()
 # Three imputs that you have to provide for backtesting:
 # 1. prices; 2. weight; 3. execution.price
-data$prices <- etf56.xts
-data$weight <- data$prices * NA
+data$prices = data$weight = data$execution.price=etf56.xts
+data$weight = data$prices * NA
 data$weight[] = 1
 data$execution.price <- data$prices
 data$execution.price[] <- NA
@@ -44,7 +44,8 @@ names(data)
 # Buy & Hold 
 models <- list()
 models$buy.hold = bt.run(data) 
-
+names(models$buy.hold)
+tail(models$buy.hold$equity)
 # you may turn off the timezone error message for just now
 # options('xts_check_TZ'=FALSE)
 
@@ -62,7 +63,7 @@ data$symbolnames <- 'tw56'
 head(data$weight, md)
 #names(data)
 #
-models$sma.cross = bt.run(data, type = 'weight', trade.summary = TRUE)  
+models$sma.cross = bt.run(data, type = 'weight', do.lag=1, trade.summary = TRUE)  
 names(models$sma.cross)
 str(models$sma.cross)
 models$sma.cross$trade.summary
